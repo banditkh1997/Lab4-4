429b565fb9d6176a2fd02b1b2d8c13ee466408ba
BanditKhawyen <banditkh1997@hotmail.com>
2020-08-27 11:19:48 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+  int per;
+  float L1,L2,area,sumL,height;
+  printf("Enter L1 = ");
+  scanf("%f",&L1);
+  printf("Enter L2 = ");
+  scanf("%f",&L2);
+  printf("Enter Height = ");
+  scanf("%f",&height);
+  per = (L2/L1)*100;
+  printf("L2 = %d\n",per);
+  sumL = L1+L2;
+  if(per >=40)
+    { area = 0.5*height*sumL;
+    printf("Area is = %0.3f",area);
+    }
+  return 0;
+}
